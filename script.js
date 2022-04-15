d7db1b50df0dd643cb7ac9e33c2f5a2c876ec446
Utkarsh Singh <sutkarsh453@gmail.com>
2022-04-15 23:39:11 +0530

    Add event listeners for number btns

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ function init(){
     readings = [];
 }
 
-const numBtns = document.querySelectorAll('#num');
-const optBtns = document.querySelectorAll('#opt');
+function makeNumber(e){
+    value = value*10 + parseInt(e.target.textContent);
+    console.log(value);
+}
+
+const numBtns = Array.from(document.querySelectorAll('#num'));
+const optBtns = Array.from(document.querySelectorAll('#opt'));
 const allClear = document.querySelector('#AC');
 const negate = document.querySelector('#negate');
 const delBtn = document.querySelector('#del');
 const display = document.querySelector('.display');
 const decimal = document.querySelector('#point');
+
+for(let i=0; i<10;i++){
+    numBtns[i].addEventListener('click', makeNumber);
+};
 
 init();
