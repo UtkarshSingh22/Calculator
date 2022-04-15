62a68596e33f1b2dd355463fb2c39ea2a68b3708
Utkarsh Singh <sutkarsh453@gmail.com>
2022-04-15 23:06:29 +0530

    Edit in ids and class of buttons

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function init(){
     value = 0;
+    readings = [];
 }
+
+const numBtns = document.querySelectorAll('#num');
+const optBtns = document.querySelectorAll('#opt');
+const allClear = document.querySelector('#AC');
+const negate = document.querySelector('#negate');
+const delBtn = document.querySelector('#del');
 
 init();
