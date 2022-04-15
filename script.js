873870acc7a28ee8351927b94a015aa643cf7a95
Utkarsh Singh <sutkarsh453@gmail.com>
2022-04-15 23:09:07 +0530

    Add query selectors for all btns and display

--- script.js
+++ script.js
@@ -8,5 +8,7 @@ const optBtns = document.querySelectorAll('#opt');
 const allClear = document.querySelector('#AC');
 const negate = document.querySelector('#negate');
 const delBtn = document.querySelector('#del');
+const display = document.querySelector('.display');
+const decimal = document.querySelector('#point');
 
 init();
